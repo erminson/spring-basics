8ad678e158012de60e2a2ef8ed9df7f1b2d96eaa
Lev Erminson <erminson@gmail.com>
2021-05-24 18:50:30 +0300

    Added App class with main method

--- src/main/java/ru/erminson/App.java
+++ src/main/java/ru/erminson/App.java
@@ -1,2 +1,21 @@
-package ru.erminson;public class App {
+package ru.erminson;
+
+public class App {
+    Client client;
+    ConsoleEventLogger eventLogger;
+
+    public void logEvent(String msg) {
+        String message = msg.replaceAll(client.getId(), client.getFullName());
+        eventLogger.logEvent(message);
+    }
+
+    public static void main(String[] args) {
+        App app = new App();
+
+        app.client = new Client("1", "John Smith");
+        app.eventLogger = new ConsoleEventLogger();
+
+        app.logEvent("Some event for user 1");
+
+    }
 }
