25f8ed4ef5bcdf59c9dd102ac1784ca0720ec14b
Lev Erminson <erminson@gmail.com>
2021-05-25 18:29:16 +0300

    Refactoring App class

--- src/main/java/ru/erminson/App.java
+++ src/main/java/ru/erminson/App.java
@@ -4,21 +4,33 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ru.erminson.beans.Client;
 import ru.erminson.beans.Event;
+import ru.erminson.beans.EventType;
 import ru.erminson.loggers.EventLogger;
 
-public class App {
-    final private Client client;
-    final private EventLogger eventLogger;
+import java.util.Map;
 
-    public App(Client client, EventLogger eventLogger) {
+public class App {
+    private final Client client;
+    private EventLogger defaultLogger;
+    private Map<EventType, EventLogger> loggers;
+
+    public App(Client client, EventLogger eventLogger, Map<EventType, EventLogger> loggers) {
+        super();
         this.client = client;
-        this.eventLogger = eventLogger;
+        this.defaultLogger = eventLogger;
+        this.loggers = loggers;
     }
 
-    public void logEvent(Event event, String msg) {
+    public void logEvent(EventType eventType, Event event, String msg) {
         String message = msg.replaceAll(client.getId(), client.getFullName());
         event.setMsg(message);
-        eventLogger.logEvent(event);
+
+        EventLogger logger = loggers.get(eventType);
+        if (logger == null) {
+            logger = defaultLogger;
+        }
+
+        logger.logEvent(event);
     }
 
     public static void main(String[] args) {
@@ -27,10 +39,12 @@ public class App {
         App app = (App) ctx.getBean("app");
 
         Event event = (Event) ctx.getBean("event");
-        app.logEvent(event, "Some event for 1");
+        app.logEvent(EventType.INFO, event, "Some event for 1");
 
         event = (Event) ctx.getBean("event");
-        app.logEvent(event,"Some event for 2");
+        app.logEvent(EventType.ERROR, event,"Some event for 2");
+        app.logEvent(null, event, "Some event for 3");
+
         ctx.close();
     }
 }
