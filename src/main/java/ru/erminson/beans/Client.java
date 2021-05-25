86a29f0458e2f2ece97873ed4b8bfe556443c109
Lev Erminson <erminson@gmail.com>
2021-05-25 17:23:22 +0300

    Refactoring class Client: added property  String type

--- src/main/java/ru/erminson/beans/Client.java
+++ src/main/java/ru/erminson/beans/Client.java
@@ -3,6 +3,7 @@ package ru.erminson.beans;
 public class Client {
     private String id;
     private String fullName;
+    private String greeting;
 
     public Client(String id, String fullName) {
         this.id = id;
@@ -23,5 +24,9 @@ public class Client {
 
     public String getFullName() {
         return fullName;
+    }
+
+    public void setGreeting(String greeting) {
+        this.greeting = greeting;
     }
 }
