3bb42d3b12e854f35fbaedd39b5f6f8ec3148624
Lev Erminson <erminson@gmail.com>
2021-05-26 08:39:29 +0300

    Added Monitor class

---
+++ src/main/java/ru/erminson/util/Monitor.java
@@ -0,0 +1,11 @@
+package ru.erminson.util;
+
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
+
+public class Monitor implements ApplicationListener {
+    @Override
+    public void onApplicationEvent(ApplicationEvent event) {
+        System.out.println(event.getClass().getSimpleName() + " > " + event.getSource().toString());
+    }
+}
